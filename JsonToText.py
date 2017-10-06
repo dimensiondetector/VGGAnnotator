40f875ef773ebd3000fe7f1880a4185f339d7f44
HokuPop <passanisijd@yahoo.com>
2017-10-06 14:17:53 -0700

    Adding comments.

--- JsonToText.py
+++ JsonToText.py
@@ -1,23 +1,40 @@
 import json
 
 with open('test.json') as json_data:
-    data = json.load(json_data)
+    jsonfile = json.load(json_data)
 
-    for params in data:
-        parameterList = data[params]
-        print(parameterList)
-        for name in parameterList['filename']:
-            print(name)
+    for parameters in jsonfile:
+        #Read each parameter in the JSON
+        parameterList = jsonfile[parameters]
+
+        #Retreive filename and create filename string for text file
+        nameString = parameterList['filename']
+        nameString = nameString.replace(".jpg", ".txt")
+
+        #Create text file with write capabilities
+        f = open(nameString, 'w')
+
+        #Boolean to tell if first value in text file
+        firstLine = True
+
+        #Get each square drawn in that picture
         for square in parameterList['regions']:
             specificSquare = parameterList['regions'][square]
-            print(" \n YES")
-            print("-------")
-            print(specificSquare)
-            print("-------")
-            for k in specificSquare['region_attributes']:
-                l = specificSquare['region_attributes'][k]
-                print(l)
-            for m in specificSquare['shape_attributes']:
-                n = specificSquare['shape_attributes'][m]
-                if (n != "rect"):
-                    print(n)
+
+            #
+            for objectIdentifier in specificSquare['region_attributes']:
+                objectName = specificSquare['region_attributes'][objectIdentifier]
+                print("---")
+                print(objectName)
+                if (firstLine == True):
+                    f.write(objectName)
+                    firstLine = False
+                else:
+                    f.write("\n" + objectName)
+                print("---")
+            for objectDetails in specificSquare['shape_attributes']:
+                objectDimensions = specificSquare['shape_attributes'][objectDetails]
+                if (objectDimensions != "rect"):
+                    print(objectDimensions)
+                    f.write(" " + str(objectDimensions))
+        f.close()
